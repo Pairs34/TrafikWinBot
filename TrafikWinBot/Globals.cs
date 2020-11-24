0c200983b7d3d411e0e447b60750619db6455e99
Pairs34 <34889012+Pairs34@users.noreply.github.com>
2020-11-24 23:48:02 +0300

    Initial commit

---
+++ TrafikWinBot/Globals.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+
+namespace TrafikWinBot
+{
+    public static class Globals
+    {
+        public static void WaitForPageLoad(IWebDriver driver)
+        {
+            try
+            {
+                IJavaScriptExecutor javaScriptExecutor = (IJavaScriptExecutor)driver;
+                WebDriverWait webDriverWait = new WebDriverWait(driver, new TimeSpan(0, 0, 15));
+                webDriverWait.Until<bool>((IWebDriver wd) => javaScriptExecutor.ExecuteScript("return document.readyState")
+                    .Equals("complete"));
+            }
+            catch (Exception)
+            {
+            }
+        }
+        
+        public static IWebElement IsElementPresent(IWebDriver driver, By by)
+        {
+            IWebElement Founded = null;
+
+            try
+            {
+                WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(25));
+                IWebElement element = wait.Until(x => driver.FindElement(by));
+                if (element.Displayed)
+                {
+                    Founded = element;
+                }
+                else
+                {
+                    Founded = null;
+                }
+            }
+            catch (Exception)
+            {
+            }
+
+            return Founded;
+        }
+        
+        public static void RunJSCommand(IWebDriver driver, string jsCommand, object[] options = null)
+        {
+            IJavaScriptExecutor javaScriptExecutor = (IJavaScriptExecutor)driver;
+            if (options != null)
+            {
+                javaScriptExecutor.ExecuteScript(jsCommand, options);
+            }
+            else
+            {
+                javaScriptExecutor.ExecuteScript(jsCommand);
+            }
+        }
+    }
+}
---
+++ TrafikWinBot/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Threading;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace TrafikWinBot
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var options = new ChromeOptions();
+            options.AddArgument("--autoplay-policy=no-user-gesture-required");
+            IWebDriver driver = new ChromeDriver(options);
+            driver.Navigate().GoToUrl("https://trafikwin.com/2020/index.php");
+            Globals.WaitForPageLoad(driver);
+            
+            Thread.Sleep(TimeSpan.FromSeconds(3));
+            
+            var user = Globals.IsElementPresent(driver,By.Name("user"));
+            user.SendKeys("forlife34@yandex.com");
+            var pass = Globals.IsElementPresent(driver,By.Name("password"));
+            pass.SendKeys("F893y010");
+            var btnConnect = Globals.IsElementPresent(driver,By.Name("connect"));
+            btnConnect.Click();
+
+            Globals.WaitForPageLoad(driver);
+
+            while (true)
+            {
+                driver.Navigate().GoToUrl("https://trafikwin.com/2020/?page=module&md=youtube");
+
+                var allVideoLinks = driver.FindElements(By.XPath("//a[@class='visit_button']"));
+                if (allVideoLinks.Count > 0)
+                {
+                    foreach (var videoLink in allVideoLinks)
+                    {
+                        driver.Navigate().GoToUrl(videoLink.GetAttribute("href"));
+
+                        Globals.WaitForPageLoad(driver);
+
+                        Globals.RunJSCommand(driver, "played = 20; roundedPlayed = 20;");
+
+                        Thread.Sleep(TimeSpan.FromSeconds(3));
+
+                        Globals.RunJSCommand(driver, "YouTubePlayed();");
+                    }
+
+                    Thread.Sleep(TimeSpan.FromSeconds(10));
+                }
+                else
+                {
+                    Console.WriteLine("Video Kalmadı. 1 Saat beklenicek");
+                    Console.WriteLine(DateTime.Now.ToShortTimeString());
+                    Thread.Sleep(TimeSpan.FromHours(1));
+                }
+            }
+        }
+    }
+}
